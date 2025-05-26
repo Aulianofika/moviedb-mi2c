ed398624dff8886a9c684d076643835e4c673aed
Aulia Nofika <aulianofika514@gmail.com>
2025-05-26 15:55:05 +0700

    input data

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MovieController::class, 'homepage'])->name('homepage');
 Route::get('/movie/{id}', [MovieController::class, 'show'])->name('movie.detail');
+Route::get('/create-movie', [MovieController::class, 'create']);
+Route::post('/movie-store', [MovieController::class, 'store']);
