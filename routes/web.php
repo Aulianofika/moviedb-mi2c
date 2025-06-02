9d1e458af2da8eb96b08d24ad54cc3dfa4bf5bc4
Aulia Nofika <aulianofika514@gmail.com>
2025-06-02 15:45:16 +0700

    make login and logout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MovieController;
+use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -9,5 +11,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MovieController::class, 'homepage'])->name('homepage');
 Route::get('/movie/{id}', [MovieController::class, 'show'])->name('movie.detail');
-Route::get('/create-movie', [MovieController::class, 'create']);
-Route::post('/movie-store', [MovieController::class, 'store']);
+Route::get('/create-movie', [MovieController::class, 'create'])->middleware('auth');
+Route::post('/movie-store', [MovieController::class, 'store'])->middleware('auth');
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
